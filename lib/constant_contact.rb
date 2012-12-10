0558e71b95bf235767971d18995bdb8092774a4d
Tom Talbott <tom.talbott@gmail.com>
2012-12-10 12:54:55 -0800

    Add Oauth 2.0 Authorization

--- lib/constant_contact.rb
+++ lib/constant_contact.rb
@@ -21,12 +21,40 @@ module ConstantContact
         basic_auth "#{api_key}%#{user}", pass
         base_uri "https://api.constantcontact.com/ws/customers/#{user.downcase}"
       end
+
+      def setup_oauth( user, token )
+      #  headers 'Authorization' => "Bearer #{token}"
+        default_params :access_token => token
+        base_uri "https://api.constantcontact.com/ws/customers/#{user.downcase}"
+      end
+
+      def get_authorize_url( api_key, redirect_uri )
+        "https://oauth2.constantcontact.com/oauth2/oauth/siteowner/authorize?response_type=code&client_id=#{api_key}&redirect_uri=#{redirect_uri}"
+      end
+
+      def authorize_code( api_key, api_secret, code, redirect_uri)
+        debug_on
+        base_uri "https://oauth2.constantcontact.com"
+        data = get('/oauth2/oauth/token', 
+          { :query => 
+              { :grant_type => 'authorization_code', 
+                :client_id => api_key, 
+                :client_secret => api_secret, 
+                :code => code, 
+                :redirect_uri => redirect_uri},
+            :format => :json})
+        if data.code == 200 # success
+          return data #ActiveSupport::JSON.decode(data.body)
+        else
+          raise BaseResource.create_exception(data)
+        end
+      end
       
       def debug_on
-        debug_output $stderr        
+        debug_output $stderr
       end
     end
-    
+
     class Error < StandardError  
       def initialize(message, code)
         super
